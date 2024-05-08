635651770706667411fead2a1ed0984e43c029ed
sdwestwood <70946774+sdwestwood@users.noreply.github.com>
2024-05-08 23:19:23 +0100

    initialised cond properly

--- TOOLS/runMNT/src/dataset.py
+++ TOOLS/runMNT/src/dataset.py
@@ -106,6 +106,7 @@ class Multimodal_Datasets(Dataset): # I have set split default to True, adjusted
         self.data_info = []
         self.labels = []
         self.acc = []
+        self.cond = []
         self.eeg = []
         # self.features = []
         self.eye = []
